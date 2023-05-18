c5fa22a5851778c9567b898f6f6229cd4ef195c0
kevidryon2 <69092380+kevidryon2@users.noreply.github.com>
2023-05-18 09:37:02 +0200

    KR Graphics v0.01

---
+++ graphics/src/krgfx.c
@@ -0,0 +1,19 @@
+#include "spng.h"
+
+const char qoi_magic[4] = "qoif";
+const char png_magic[4] = "\x89PNG";
+
+int main(int argc, char **argv) {
+	if (argc<3) {
+		printf("Usage: %s <file>\nThe script gets read from STDIN. Use a tool such as 'cat' to use a file as a script.\n", argv[0]);
+		return 1;
+	}
+	
+	char scbuff[BUFSIZ];
+	
+	fread(scbuff, 1, BUFSIZ, stdin);
+	
+	
+	
+	return 0;
+}
